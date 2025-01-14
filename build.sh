6776c3773b753cb872f818cbb07c78a527fbaaa5
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2025-01-15 00:58:17 +1100

    Generate navbars with current page highlighted instead of always home

--- build.sh
+++ build.sh
@@ -7,11 +7,36 @@ PAGES="home about membership calendar"
 
 mkdir -p output/assets/
 
-for page in $PAGES; do
+# TODO: Set html language
+# TODO: Avoid user-scalable=yes on viewport meta element
+for output_page in $PAGES; do
+  navbar="\n"
+  for navbar_page in $PAGES; do
+    name=$(sed -n 's/^pagetitle: //p' pages/"$navbar_page".md)
+    if [ -z "$name" ]; then
+      name=$(sed -n 's/^title: //p' pages/"$navbar_page".md)
+    fi
+    if [ -z "$name" ]; then
+      printf "Skipping %s due to missing yaml title\n" "$pages/$output_page.md"
+      continue
+    fi
+
+    navbar="$navbar            <li class=\"nav-item mb-2 px-2\">\n"
+    navbar="$navbar              <a class=\"nav-link"
+    if [ "$output_page" = "$navbar_page" ]; then
+      navbar="$navbar active\" aria-current=\"page\" href=\"#"
+    else
+      navbar="$navbar\" href=\"$navbar_page.html"
+    fi
+    navbar="$navbar\">$name</a>\n"
+    navbar="$navbar            </li>\n"
+  done
+  navbar="$navbar          "
+
   # shellcheck disable=SC2086
-  $PANDOC $ARGS "pages/$page.md" -o "output/$page.html"
-  sed -i.tmp -n '/empty.css/!p' "output/$page.html"
-  rm "output/$page.html.tmp"
+  $PANDOC $ARGS "pages/$output_page.md" -o "output/$output_page.html"
+  sed -i.tmp  -e "s\`%NAVBAR_ITEMS%\`$navbar\`" -n -e '/empty.css/!p' "output/$output_page.html"
+  rm "output/$output_page.html.tmp"
 done
 
 cp -r assets/style.css output/assets/style.css
