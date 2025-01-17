a569d56a1fc63525b1108847a1636db0f9a97ca0
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2025-01-17 19:52:53 +1100

    Fix gettimezone.sh erroring sometimes due to octal numbers being detected

--- gettimezone.sh
+++ gettimezone.sh
@@ -3,7 +3,8 @@
 # Based on https://stackoverflow.com/a/41643684
 
 T='+%j*1440+%H*60+%M' # minutes in year: DAY/Y * 1440 min/d + H * 60 h/min + MIN
-Z=$(( ( $(date "$T") - ( $(date -u "$T") ) ) * 100 / 60 )) # TZ offset as hr*100
+SED_ARG='s/^0\{1,\}\|\(+\)0\{1,\}\([[:digit:]]\)/\1\2/g'
+Z=$(( ( $(date "$T" | sed "$SED_ARG") - ( $(date -u "$T" | sed "$SED_ARG") ) ) * 100 / 60 )) # TZ offset as hr*100
 
 H=${Z%??} # hours ($Z is hundredths of hours, so we remove the last two digits)
 if   [ "$H" -lt -13 ]; then H=$((H+8712))    # UTC is a year ahead
